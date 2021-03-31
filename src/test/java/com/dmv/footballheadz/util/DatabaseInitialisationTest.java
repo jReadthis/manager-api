ccee4b0a90a49779b2a4b45f79f9f6ec15b74f9e
jReadthis <o.marca25@gmail.com>
2021-03-31 18:47:59 -0400

    init commit

---
+++ src/test/java/com/dmv/footballheadz/util/DatabaseInitialisationTest.java
@@ -0,0 +1,89 @@
+package com.dmv.footballheadz.util;
+
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.model.*;
+import com.dmv.footballheadz.season.impl.Season;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static com.amazonaws.services.dynamodbv2.model.TableStatus.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(SpringExtension.class)
+class DatabaseInitialisationTest {
+
+    @Mock
+    private DynamoDBMapper dbMapper;
+
+    @Mock
+    private AmazonDynamoDB dynamoDB;
+
+    @InjectMocks
+    private DatabaseInitialisation databaseInitialisation;
+
+    private final String tableName = "Season";
+    private final CreateTableRequest createTableRequest = new CreateTableRequest().withTableName(tableName);
+
+    @BeforeEach
+    public void setUp() throws Exception {
+
+        when(dbMapper.generateCreateTableRequest(Season.class)).thenReturn(createTableRequest);
+        when(dynamoDB.createTable(createTableRequest))
+                .thenReturn(new CreateTableResult().withTableDescription(tableDescriptionWithStatus(CREATING)));
+    }
+
+    @Test
+    public void shouldNotCreateTableIfAlreadyExists() throws Exception {
+
+        when(dynamoDB.describeTable(tableName))
+                .thenReturn(new DescribeTableResult().withTable(tableDescriptionWithStatus(ACTIVE)));
+        databaseInitialisation.onApplicationEvent(null);
+        verify(dynamoDB, never()).createTable(any(CreateTableRequest.class));
+    }
+
+    @Test
+    public void shouldNotCreateTableIfTableCreationInProgress() throws Exception {
+
+        when(dynamoDB.describeTable(tableName))
+                .thenReturn(new DescribeTableResult().withTable(tableDescriptionWithStatus(CREATING)));
+        databaseInitialisation.onApplicationEvent(null);
+        verify(dynamoDB, never()).createTable(any(CreateTableRequest.class));
+    }
+
+    @Test
+    public void shouldNotCreateTableIfTableDeletionInProgress() throws Exception {
+
+        when(dynamoDB.describeTable(tableName))
+                .thenReturn(new DescribeTableResult().withTable(tableDescriptionWithStatus(DELETING)));
+        databaseInitialisation.onApplicationEvent(null);
+        verify(dynamoDB, never()).createTable(any(CreateTableRequest.class));
+    }
+
+    @Test
+    public void shouldNotCreateTableIfTableUpdateInProgress() throws Exception {
+
+        when(dynamoDB.describeTable(tableName))
+                .thenReturn(new DescribeTableResult().withTable(tableDescriptionWithStatus(UPDATING)));
+        databaseInitialisation.onApplicationEvent(null);
+        verify(dynamoDB, never()).createTable(any(CreateTableRequest.class));
+    }
+
+    @Test
+    public void shouldCreateTableIfTableDoesNotExist() throws Exception {
+
+        when(dynamoDB.describeTable(tableName)).thenThrow(new ResourceNotFoundException("Simulated failure"));
+        databaseInitialisation.onApplicationEvent(null);
+        verify(dynamoDB).createTable(createTableRequest);
+    }
+
+    private TableDescription tableDescriptionWithStatus(TableStatus status) {
+
+        return new TableDescription().withTableStatus(status).withTableName(tableName);
+    }
+}
